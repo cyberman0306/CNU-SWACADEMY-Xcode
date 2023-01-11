6631d70356b3a12cf9bada77f0c8367f55bc2ea4
HYUNJUN YOOK <yhj03069@gmail.com>
2023-01-11 14:02:21 +0900

    Initial Commit

---
+++ week2_practice/week2_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  week2_practiceApp.swift
+//  week2_practice
+//
+//  Created by cnu on 2023/01/11.
+//
+
+import SwiftUI
+
+@main
+struct week2_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
