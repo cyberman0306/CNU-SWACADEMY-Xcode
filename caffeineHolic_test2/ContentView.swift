75b24ee26c3be7ece8ec996066f305ba178c70e5
HYUN JUN YOOK <yhj03069@gmail.com>
2023-01-17 09:58:45 +0900

    init

--- caffeineHolic_test2/ContentView.swift
+++ caffeineHolic_test2/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State private var dailyList = ["First", "Second"]
+    
+    func incList() {
+        dailyList.insert("New", at: 0)
+    }
+    
+    var DailyView: some View {
+        HStack(alignment: .top) {
+            VStack(alignment: .leading, spacing: 20) {
+                List(dailyList, id: \.self) { item in
+                    Text(item)
+                }
+                Spacer()
+                Text("")
+            }
+            .frame(width: 200)
+            .opacity(0.3)
+            Spacer()
         }
         .padding()
+    }
+    
+    var Buttons: some View {
+        VStack {
+            HStack (spacing: 20) {
+                Text("")
+                Spacer()
+                Text("Top button")
+            }
+            Spacer()
+            Button("Go to Caffeine") {
+                print("Click")
+                incList()
+            }
+            Spacer()
+            Text("Button")
+        }
+        .padding()
+    }
+    
+
+    
+    var body: some View {
+        ZStack {
+            DailyView
+            Buttons
+        }
     }
 }
 
