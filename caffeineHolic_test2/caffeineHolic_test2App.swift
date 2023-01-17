1c404699993e5dea6dc961f7f0afda117de89fed
HYUN JUN YOOK <yhj03069@gmail.com>
2023-01-17 09:44:50 +0900

    Initial Commit

---
+++ caffeineHolic_test2/caffeineHolic_test2App.swift
@@ -0,0 +1,17 @@
+//
+//  caffeineHolic_test2App.swift
+//  caffeineHolic_test2
+//
+//  Created by cnu on 2023/01/17.
+//
+
+import SwiftUI
+
+@main
+struct caffeineHolic_test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
